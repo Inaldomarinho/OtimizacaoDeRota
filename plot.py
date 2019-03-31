e0fee0c502be8c1ff6f1d7a31403058f0047e4e0
inaldomarinho <inaldomarinhoqueirozneto@gmail.com>
2019-03-31 15:51:47 -0300

    31 mar 2019

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
-def plot(caminho):
+def plot(caminho, grafo):
+    plt.figure(1)
     im = plt.imread("mapa.PNG")
     plt.imshow(im, zorder=1)
     x = []
@@ -10,5 +11,28 @@ def plot(caminho):
         x.append(no.x)
         y.append(no.y)
 
+    plotNos(grafo, plt)
     plt.plot(x, y, label='line 2')
+
+    plotCaminhosPossiveis(grafo, plt)
+
+    plt.show()
+
+def plotNos(grafo, plot):
+
+    for no in grafo:
+        plot.scatter(no.x, no.y, zorder=3, color='#1f77b4')
+
+
+
+def plotCaminhosPossiveis(grafo, plt):
+
+    plt.figure(2)
+    im = plt.imread("mapa.PNG")
+    plt.imshow(im, zorder=1)
+
+    for no in grafo:
+        for vizinho in no.vizinhos:
+            plt.plot([no.x, vizinho.x], [no.y, vizinho.y], label='line 2')
+
     plt.show()
